06b862f7cd01eec1b6c852a678002bdb46a30365
oznetmaster <ncolvin@nivloc.com>
2018-06-11 06:00:17 +0800

    Initial port of the BigMath library to S#

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs">
+//   Copyright (c) 2013 Alexander Logger. All rights reserved.
+//   Copyright (c) 2018 Nivloc Enterprises Ltd. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("BigMath")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyCompany ("Nivloc Enterprises Ltd")]
+[assembly: AssemblyCopyright ("Copyright © 2018 Nivloc Enterprises Ltd")]
---
+++ README.md
@@ -0,0 +1,9 @@
+# BigMath
+
+S# class library for computations with big numbers such as `Int128`, `Int256` and `BigInteger`. There is also `ExtendedBitConverter` which allows to convert regular and big numbers to/from array of bytes, with possibility of explicit setting of bytes order [Big-endian/Little-endian](http://en.wikipedia.org/wiki/Endianness).
+
+Some parts of the code is based on:
+
+1. Original sources from [https://github.com/everbytes/BigMath](https://github.com/everbytes/BigMath) and [http://int128.codeplex.com/](http://int128.codeplex.com/). Thanks to [Simon Mourier](https://www.codeplex.com/site/users/view/simonm).
+2. `BigInteger` from the [Bouncy Castle](http://www.bouncycastle.org/). 
+
